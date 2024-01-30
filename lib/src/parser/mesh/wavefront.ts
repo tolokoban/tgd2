055ae3c527e0ea48cb8139d2a6d0d4640c5691a9
Fabien PETITJEAN <fabien.petitjean@epfl.ch>
2024-01-30 18:57:19 +0100

    Wavefront parser

--- lib/src/parser/mesh/wavefront.ts
+++ lib/src/parser/mesh/wavefront.ts
@@ -3,12 +3,14 @@ import { forEachLine } from "../for-each-line"
 export interface TgdParserMeshWavefrontOptions {
     onVertex(x: number, y: number, z: number): void
     onNormal(x: number, y: number, z: number): void
-    onUV(u: number, v: number): void
-    onFace(faces: Array<{
-        vertex: number,
-        normal?: number
-        uv?: number
-    }>): void
+    onTexture(u: number, v?: number, w?: number): void
+    onFace(
+        faces: Array<{
+            vertex: number
+            normal?: number
+            uv?: number
+        }>
+    ): void
     onObject(name: string): void
     onGroup(name: string): void
 }
@@ -19,34 +21,56 @@ export interface TgdParserMeshWavefrontOptions {
  * Textures and normals are discarded since we don't need them.
  */
 export class TgdParserMeshWavefront {
-    parse(content: string,options:Partial<TgdParserMeshWavefrontOptions>
-        ): {
-    vertices: [x: number, y: number, z: number][]
-    triangles: [v0: number, v1: number, v2: number][]
-} {
-    for (const fullLine of forEachLine(content)) {
-        const line = fullLine.trimStart()
-        if (line.startsWith("v ")) {
-            const vertex = line
-                .substring("v ".length)
-                .split(" ")
-                .map(v => Number(v))
-            if (isVector3(vertex)) vertices.push(vertex)
+    constructor(
+        private readonly options: Partial<TgdParserMeshWavefrontOptions>
+    ) {}
+
+    parse(
+        content: string,
+        options: Partial<TgdParserMeshWavefrontOptions> = {}
+    ): void {
+        const { onVertex, onNormal, onTexture, onFace } = {
+            ...this.options,
+            ...options,
         }
-        if (line.startsWith("f ")) {
-            const triangle = line
-                .substring("f ".length)
-                .split(" ")
-                // Warning! We need to remove 1 to the index.
-                .map(v => Number(v) - 1)
-            if (isVector3(triangle)) triangles.push(triangle)
-            else
-                throw Error(
-                    `This simple Wavefront parser accepts only triangles!\n${line}`
+        for (const fullLine of forEachLine(content)) {
+            const line = fullLine.trimStart()
+            if (onVertex && line.startsWith("v ")) {
+                const vertex = line
+                    .substring("v ".length)
+                    .split(" ")
+                    .map(v => Number(v))
+                if (isVector3(vertex)) onVertex(...vertex)
+            } else if (onFace && line.startsWith("f ")) {
+                onFace(
+                    line
+                        .substring("f ".length)
+                        .split(" ")
+                        // Warning! We need to remove 1 to the index.
+                        .map(face => {
+                            const [v, n, t] = face.split("/")
+                            return {
+                                vertex: Number(v) - 1,
+                                normal: n ? Number(n) - 1 : undefined,
+                                uv: t ? Number(t) - 1 : undefined,
+                            }
+                        })
                 )
+            } else if (onNormal && line.startsWith("vn ")) {
+                const normal = line
+                    .substring("vn ".length)
+                    .split(" ")
+                    .map(n => Number(n))
+                if (isVector3(normal)) onNormal(...normal)
+            } else if (onTexture && line.startsWith("vt ")) {
+                const [u, v, w] = line
+                    .substring("vt ".length)
+                    .split(" ")
+                    .map(n => Number(n))
+                onTexture(u, v, w)
+            }
         }
     }
-    return { vertices, triangles }
 }
 
 function isVector3(data: number[]): data is [number, number, number] {
