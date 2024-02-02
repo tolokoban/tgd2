0c6d3e6c402845316876dad943bdef7a9f85d8a4
Tolokoban <contact@tolokoban.org>
2024-02-02 17:42:08 +0100

    Contrain axis of rotation in orbit controller

--- lib/src/controller/camera/orbit.ts
+++ lib/src/controller/camera/orbit.ts
@@ -1,8 +1,10 @@
 import { TgdCamera } from "@/camera"
+import { TdgInputKeyboard } from "@/input"
 import { TgdInputPointer, TgdInputPointerEvent } from "@/input/pointer"
 
 export class TgdControllerCameraOrbit {
     private pointer: TgdInputPointer | null = null
+    private keyboard = new TdgInputKeyboard()
 
     constructor(private readonly camera: TgdCamera) {}
 
@@ -31,14 +33,31 @@ export class TgdControllerCameraOrbit {
         previous: TgdInputPointerEvent
         start: TgdInputPointerEvent
     }) => {
-        const { camera } = this
+        const { camera, keyboard } = this
         const dt = evt.current.t - evt.previous.t
         if (dt <= 0) return
+
+        if (keyboard.isPressed("z")) {
+            const x1 = evt.previous.x
+            const y1 = evt.previous.y
+            if (Math.abs(x1) + Math.abs(y1) === 0) return
+
+            const x2 = evt.current.x
+            const y2 = evt.current.y
+            if (Math.abs(x2) + Math.abs(y2) === 0) return
+
+            const x = x1 * x2 + y1 * y2
+            const y = x1 * y2 - y1 * x2
+            const ang = Math.atan2(y, x)
+            camera.orbitAroundZ(-ang)
+            return
+        }
 
         const speed = 25 / dt
         const dx = (evt.current.x - evt.previous.x) * speed
         const dy = (evt.current.y - evt.previous.y) * speed
-        camera.orbitAroundY(-dx).orbitAroundX(dy)
+        if (!keyboard.isPressed("x")) camera.orbitAroundY(-dx)
+        if (!keyboard.isPressed("y")) camera.orbitAroundX(dy)
     }
 
     private readonly handleZoom = (
