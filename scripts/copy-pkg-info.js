1644edb6412465286aafda50808c74ef4b311b09
Fabien PETITJEAN <fabien.petitjean@epfl.ch>
2024-02-13 10:59:18 +0100

    copy pkg info script

---
+++ scripts/copy-pkg-info.js
@@ -0,0 +1,28 @@
+const FS = require("fs")
+const Path = require("path")
+
+const [, prg, source, ...targets] = process.argv
+if (
+    typeof source !== "string" ||
+    !Array.isArray(targets) ||
+    targets.length === 0
+) {
+    console.log("")
+    console.log("Usage: ", prg, "./source ./dest1 ./dest2 ...")
+    console.log("")
+    process.exit(1)
+}
+
+const src = Path.resolve(source, "package.json")
+const pkgSrc = JSON.parse(FS.readFileSync(src).toString())
+const { name, version, description, homepage } = pkgSrc
+console.log(`${name} v${version}: ${description}`)
+for (const target of targets) {
+    const dst = Path.resolve(target, "package.json")
+    const pkgDst = JSON.parse(FS.readFileSync(dst).toString())
+    pkgDst.name = name
+    pkgDst.version = version
+    pkgDst.description = description
+    pkgDst.homepage = homepage
+    FS.writeFileSync(dst, JSON.stringify(pkgDst, null, "    "))
+}
