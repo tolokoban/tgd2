5319f2e4928ad7a40234db50119db5053dfcf64c
Tolokoban <contact@tolokoban.org>
2025-02-21 17:10:28 +0100

    tgdLoadVideo

--- lib/src/loader/index.ts
+++ lib/src/loader/index.ts
@@ -1,3 +1,4 @@
 export * from "./asset"
 export * from "./binary"
 export * from "./image"
+export * from "./video"
---
+++ lib/src/loader/video.ts
@@ -0,0 +1,21 @@
+/**
+ * Try to load a video and return `null` in case of failure.
+ * @param url URL of the image to load.
+ */
+export async function tgdLoadVideo(
+    url: string
+): Promise<HTMLVideoElement | null> {
+    return new Promise(resolve => {
+        const video = document.createElement("video")
+        video.addEventListener("canplay", () => resolve(video))
+        video.onerror = () => {
+            console.error("Unable to load video: ", url)
+            resolve(null)
+        }
+        video.src = url
+        video.setAttribute("loop", "")
+        video.setAttribute("muted", "")
+        video.setAttribute("autoplay", "")
+        video.setAttribute("disablepictureinpicture", "")
+    })
+}
