d0ba96364291090a70e6ea52bb3061d24e94d050
Tolokoban <contact@tolokoban.org>
2025-06-01 20:28:18 +0200

    Wasm

--- lib/src/wasm/index.ts
+++ lib/src/wasm/index.ts
@@ -1,2 +1,25 @@
+import Wabt from "wabt"
+
+import { WasmModule } from "./language/types"
+import { wasmModule } from "./language/to-code"
+import { tgdCodeStringify } from "@tgd/shader"
+
 export type * from "./language/types"
 export * from "./language/to-code"
+
+export async function tgdWasmCompile(module: WasmModule) {
+    const wabt = await Wabt()
+    const code = tgdCodeStringify(wasmModule(module))
+    const compiler = wabt.parseWat("tgd2.wat", code)
+    compiler.resolveNames()
+    compiler.validate()
+    const binary = compiler.toBinary({
+        log: true,
+        write_debug_names: true,
+    })
+    const { buffer } = binary
+    compiler.destroy()
+    const compiledModule = await WebAssembly.compile(buffer)
+    const instance = await WebAssembly.instantiate(compiledModule, importObject)
+    return instance
+}
