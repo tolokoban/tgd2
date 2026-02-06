adc82c0b44855694f91258fa0c70cbcb0b8abc9b
Tolokoban <contact@tolokoban.org>
2026-02-06 22:28:37 +0100

    GeometryPlane can now set UVs:

--- lib/src/geometry/plane.ts
+++ lib/src/geometry/plane.ts
@@ -1,6 +1,6 @@
 import { TgdDataset } from "@tgd/dataset"
 import { TgdGeometry } from "./geometry"
-import { ArrayNumber3 } from "@tgd/types"
+import { ArrayNumber2, ArrayNumber3 } from "@tgd/types"
 import { TgdVec3 } from "@tgd/math"
 
 export interface TgdGeometryPlaneOptions {
@@ -8,6 +8,8 @@ export interface TgdGeometryPlaneOptions {
     sizeY?: number
     vecX?: ArrayNumber3
     vecY?: ArrayNumber3
+    uv0?: ArrayNumber2
+    uv1?: ArrayNumber2
 }
 
 export class TgdGeometryPlane extends TgdGeometry {
@@ -16,6 +18,8 @@ export class TgdGeometryPlane extends TgdGeometry {
         sizeY = 1,
         vecX = [1, 0, 0],
         vecY = [0, 1, 0],
+        uv0 = [0, 0],
+        uv1 = [1, 1],
     }: TgdGeometryPlaneOptions = {}) {
         const vec = (x: number, y: number): ArrayNumber3 => {
             const sx = x * sizeX
@@ -39,12 +43,14 @@ export class TgdGeometryPlane extends TgdGeometry {
             ...vec(+H, +H),
             ...vec(-H, +H),
         ]))
+        const [u0, v0] = uv0
+        const [u1, v1] = uv1
         // prettier-ignore
         dataset.set("TEXCOORD_0", new Float32Array([
-            1, 1,
-            0, 1,
-            1, 0,
-            0, 0,
+            u1, v1,
+            u0, v1,
+            u1, v0,
+            u0, v0,
         ]))
         const [nx, ny, nz] = new TgdVec3(vecX)
             .normalize()
