647189c6003e3deb40b05b4bbecef3b52c868a04
Tolokoban <contact@tolokoban.org>
2026-03-23 19:30:32 +0100

    GLTF material

---
+++ doc/src/app/articles/state/blend/_/add.demo/add.demo.tsx
@@ -0,0 +1,79 @@
+import {
+    TgdCameraPerspective,
+    TgdContext,
+    TgdGeometryPlane,
+    TgdMaterialFlat,
+    TgdPainterBackground,
+    TgdPainterClear,
+    TgdPainterMesh,
+    TgdPainterState,
+    TgdTexture2D,
+} from "@tolokoban/tgd"
+import View, { Assets } from "@/components/demo/Tgd"
+
+import BackgroundURL from "@/assets/image/dino.webp"
+import SuzanneURL from "@/assets/mesh/suzanne.glb"
+
+function init(context: TgdContext, assets: Assets) {
+    // #begin
+    const camera = new TgdCameraPerspective()
+    camera.spaceHeightAtTarget = 2
+    context.camera = camera
+    const planes = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1],
+    ].map(([R, G, B], index) => makePlane(context, R, G, B, index))
+    const state = new TgdPainterState(context, {
+        blend: "add",
+        children: planes,
+    })
+    context.add(
+        new TgdPainterClear(context, { color: [0, 0, 0, 1], depth: 1 }),
+        new TgdPainterBackground(context, {
+            texture: new TgdTexture2D(context).loadBitmap(assets.image.background),
+        }),
+        state,
+    )
+    context.paint()
+    // #end
+}
+
+export default function Demo() {
+    return (
+        <View
+            onReady={init}
+            options={{
+                depth: true,
+                alpha: false,
+                antialias: true,
+            }}
+            assets={{
+                glb: {
+                    suzanne: SuzanneURL,
+                },
+                image: {
+                    background: BackgroundURL,
+                },
+            }}
+        />
+    )
+}
+
+function makePlane(context: TgdContext, R: number, G: number, B: number, index: number) {
+    const angle = index * 0.5 * Math.PI
+    const radius = 0.6
+    const x = radius * Math.cos(angle)
+    const y = radius * Math.sin(angle)
+    console.log("🐞 [blend.demo@68] x, y =", x, y) // @FIXME: Remove this line written on 2026-03-20 at 18:38
+    return new TgdPainterMesh(context, {
+        transfo: {
+            position: [x, y, 0],
+        },
+        geometry: new TgdGeometryPlane(),
+        material: new TgdMaterialFlat({
+            color: [R, G, B, 0.6],
+        }),
+    })
+}
---
+++ doc/src/app/articles/state/blend/_/add.demo/index.tsx
@@ -0,0 +1,25 @@
+/**
+ * Warning! This code has been generated automatically.
+ */
+import React from "react"
+import { ViewButton } from "@tolokoban/ui"
+import CodeViewer from "@/components/demo/CodeViewer"
+import Demo from "./add.demo"
+
+const FOCUS = {"Detail #1":"    const camera = new TgdCameraPerspective()\n    camera.spaceHeightAtTarget = 2\n    context.camera = camera\n    const planes = [\n        [1, 0, 0],\n        [0, 1, 0],\n        [0, 0, 1],\n        [1, 1, 1],\n    ].map(([R, G, B], index) => makePlane(context, R, G, B, index))\n    const state = new TgdPainterState(context, {\n        blend: \"add\",\n        children: planes,\n    })\n    context.add(\n        new TgdPainterClear(context, { color: [0, 0, 0, 1], depth: 1 }),\n        new TgdPainterBackground(context, {\n            texture: new TgdTexture2D(context).loadBitmap(assets.image.background),\n        }),\n        state,\n    )\n    context.paint()"}
+const FULL = "import {\n    TgdCameraPerspective,\n    TgdContext,\n    TgdGeometryPlane,\n    TgdMaterialFlat,\n    TgdPainterBackground,\n    TgdPainterClear,\n    TgdPainterMesh,\n    TgdPainterState,\n    TgdTexture2D,\n} from \"@tolokoban/tgd\"\nimport View, { Assets } from \"@/components/demo/Tgd\"\n\nimport BackgroundURL from \"@/assets/image/dino.webp\"\nimport SuzanneURL from \"@/assets/mesh/suzanne.glb\"\n\nfunction init(context: TgdContext, assets: Assets) {\n    const camera = new TgdCameraPerspective()\n    camera.spaceHeightAtTarget = 2\n    context.camera = camera\n    const planes = [\n        [1, 0, 0],\n        [0, 1, 0],\n        [0, 0, 1],\n        [1, 1, 1],\n    ].map(([R, G, B], index) => makePlane(context, R, G, B, index))\n    const state = new TgdPainterState(context, {\n        blend: \"add\",\n        children: planes,\n    })\n    context.add(\n        new TgdPainterClear(context, { color: [0, 0, 0, 1], depth: 1 }),\n        new TgdPainterBackground(context, {\n            texture: new TgdTexture2D(context).loadBitmap(assets.image.background),\n        }),\n        state,\n    )\n    context.paint()\n}\n\nexport default function Demo() {\n    return (\n        <View\n            onReady={init}\n            options={{\n                depth: true,\n                alpha: false,\n                antialias: true,\n            }}\n            assets={{\n                glb: {\n                    suzanne: SuzanneURL,\n                },\n                image: {\n                    background: BackgroundURL,\n                },\n            }}\n        />\n    )\n}\n\nfunction makePlane(context: TgdContext, R: number, G: number, B: number, index: number) {\n    const angle = index * 0.5 * Math.PI\n    const radius = 0.6\n    const x = radius * Math.cos(angle)\n    const y = radius * Math.sin(angle)\n    console.log(\"🐞 [blend.demo@68] x, y =\", x, y) // @FIXME: Remove this line written on 2026-03-20 at 18:38\n    return new TgdPainterMesh(context, {\n        transfo: {\n            position: [x, y, 0],\n        },\n        geometry: new TgdGeometryPlane(),\n        material: new TgdMaterialFlat({\n            color: [R, G, B, 0.6],\n        }),\n    })\n}\n"
+
+export default function DemoContainer() {
+    const [full, setFull] = React.useState(false)
+    return <>
+        <div className="half-left"><Demo /></div>
+        <div className="half-right">
+            <div>
+                <ViewButton variant="elevated" onClick={() => setFull(!full)}>
+                    {full ? "Show code details" : "Show full code"}
+                </ViewButton>
+            </div>
+            <CodeViewer language="tsx" value={full ? FULL : FOCUS} />
+        </div>
+    </>
+}
